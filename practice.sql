7cd5d1052ee1fd6b18a25aff181028981e0211b0
sw883@cornell.edu <sw883@cornell.edu>
2017-04-25 15:26:50 -0400

    type of triangle

--- practice.sql
+++ practice.sql
@@ -1,3 +1,15 @@
 #revising the select query
 select * from CITY
 where COUNTRYCODE = 'USA' and POPULATION > 100000;
+
+#type of triangle
+select
+case
+   when (A+B)>C and (B+C)>A and (A+C)>B  
+   then (case when A=B and B=C then 'Equilateral'
+         when A <> B and B <> C and C <> A then 'Scalene'
+         else 'Isosceles'
+    end)
+   else 'Not A Triangle'
+end 
+from TRIANGLES;
