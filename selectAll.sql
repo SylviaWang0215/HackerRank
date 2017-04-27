3d932115120ace52d523ffd3daeedbc87b6828fa
sw883@cornell.edu <sw883@cornell.edu>
2017-04-27 02:03:27 -0400

    select all
    1. use CASE WHEN in the order by
    2. can not use ORDER BY twice for multiply queues in UNION
    3. practice for LEFT JOIN

--- selectAll.sql
+++ selectAll.sql
@@ -64,3 +64,41 @@ where substr(lower(CITY), 1, 1) in ('e', 'a', 'i', 'o', 'u')
 and substr(lower(CITY), -1) in ('e', 'a', 'i', 'o', 'u');
 
 
+select distinct CITY from STATION
+where substr(lower(CITY), 1, 1) not in ('e', 'a', 'i', 'o', 'u');
+
+select distinct CITY from STATION 
+where substr(lower(CITY), -1) not in ('a', 'e', 'i', 'o', 'u');
+
+
+select distinct CITY from STATION 
+where substr(lower(CITY), -1, 1) not in ('a', 'e', 'i', 'o', 'u')
+or substr(lower(CITY), 1, 1) not in ('a', 'e', 'i', 'o', 'u');
+
+
+select Name from STUDENTS
+where Marks > 75
+order by substr(Name, -3, 3) asc, ID asc;
+
+
+select name from Employee
+where salary > 2000 and months < 10
+order by employee_id asc;
+
+
+select COUNTRY.CONTINENT as continent, floor(avg(CITY.POPULATION)) as avg_population
+from COUNTRY inner join CITY
+on CITY.COUNTRYCODE = COUNTRY.CODE
+group by COUNTRY.CONTINENT;
+
+
+select case 
+when Grade >= 8 then Name
+else 'NULL' end as name, Grade, Marks from 
+Students inner join Grades on Marks between Min_Mark and Max_Mark
+Order by Grade desc,
+case when Grade >= 8 Then Grade end desc,
+case when Grade >= 8 Then Name end asc,
+case when Grade < 8 then Marks end asc;
+
+
