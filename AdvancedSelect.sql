134d407a1f81e617daabb469ff824c0d8685853b
sw883@cornell.edu <sw883@cornell.edu>
2017-04-30 01:48:14 -0400

    select

--- AdvancedSelect.sql
+++ AdvancedSelect.sql
@@ -1,4 +1,3 @@
 select Name||'('||substr(Occupation, 1, 1)||')' as output from OCCUPATIONS order by Name;
 
 select 'There are total '||count(Occupation)||' '||lower(Occupation)||'s.' as sentence from Occupations group by occupation order by count(Occupation) asc , occupation;
-
